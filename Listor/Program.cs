384d2b7b63ca16736883be2ad813b052df7aca4b
Fridaychill <nicholasleon81@gmail.com>
2026-09-23 15:40:00 +0200

    Add project files.

---
+++ Listor/Program.cs
@@ -0,0 +1,57 @@
+﻿string[] listas = new string[10];
+listas[2] = "Bajs";
+
+for (int i = 0; i < listas.Length; i++)
+{
+    var currentItem = listas[i];
+    if (!string.IsNullOrWhiteSpace(currentItem))
+    {
+        Console.WriteLine(listas[i]);
+    }
+}
+
+List<string> unknownlenght =
+[
+    "enhet1", "enhet2", "enhet3"
+];
+
+// var myNewArray = unknownlenght.ToArray();
+while (true)
+{
+    Console.WriteLine("Vill du lägga till en ny produkt?");
+    Console.WriteLine("Ifall du vill gå ut ur programmet skriv: 5");
+    string input = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        Console.Clear();
+        Console.WriteLine("Skriv något giltigt");
+    }
+    if (input == "5")
+    {
+        break;
+    }
+    else
+    {
+        Console.Clear();
+        unknownlenght.Add(input);
+    }
+}
+
+foreach (string saker in unknownlenght)
+{
+    Console.WriteLine($"{saker}");
+
+}
+
+
+
+
+try
+{
+    listas[11] = "Yippe!";
+
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Wrong!!! : {e.Message}");
+}
