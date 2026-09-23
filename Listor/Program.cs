e0f079ea28a13e896cf96f003ee022408ce88bc2
Fridaychill <nicholasleon81@gmail.com>
2026-09-23 15:47:52 +0200

    Lite lätt vad vi gick igenom med kommentarer

--- Listor/Program.cs
+++ Listor/Program.cs
@@ -1,21 +1,26 @@
-﻿string[] listas = new string[10];
-listas[2] = "Bajs";
+﻿//enkelt sätt att skapa en array med 10 platser. specifikt en string i detta fall kan vara Int , double, bool etc.
+string[] listas = new string[10];
+//specifierar att index 2 ska ha värdet "Kul"
+listas[2] = "Kul";
 
+//for loop som går igenom alla index i arrayen och skriver ut värdet på indexet om det inte är null, tomt eller whitespace.
 for (int i = 0; i < listas.Length; i++)
 {
     var currentItem = listas[i];
+    // IsNullOrWhiteSpace kollar om det är null, tomt eller whitespace(alltså inget värde).
     if (!string.IsNullOrWhiteSpace(currentItem))
     {
         Console.WriteLine(listas[i]);
     }
 }
 
+//enkelt sätt att skapa en lista med okänd längd. Listor är dynamiska och kan växa och krympa i storlek så länge man vill.
 List<string> unknownlenght =
 [
     "enhet1", "enhet2", "enhet3"
 ];
 
-// var myNewArray = unknownlenght.ToArray();
+//while loop som frågar användaren om den vill lägga till en ny produkt i listan. Ifall användaren skriver 5 så avslutas loopen och programmet.
 while (true)
 {
     Console.WriteLine("Vill du lägga till en ny produkt?");
@@ -45,7 +50,7 @@ foreach (string saker in unknownlenght)
 
 
 
-
+//try catch medod som tar hand on ifall du sätter in ett index som inte finns i arrayen.
 try
 {
     listas[11] = "Yippe!";
